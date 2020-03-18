3fcc7a33545f7998c4f21199faa4318dc5007b1e
jinzlgit <1436315008@qq.com>
2020-03-18 11:08:58 +0800

    SpringBoot集成es项目创建

---
+++ src/main/java/com/es/boot/BootEsApplication.java
@@ -0,0 +1,13 @@
+package com.es.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootEsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/es/boot/BootEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
